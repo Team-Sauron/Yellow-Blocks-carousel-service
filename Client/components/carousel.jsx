0445363e97ba8218949286099e61ee5ccc9b78b6
Jacob Orel <jakeorel@gmail.com>
2020-04-29 22:22:31 -0500

    added basic skeleton

---
+++ Client/components/carousel.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+  render() {
+    return (
+      <div>IM INSIDE REACT</div>
+    );
+  };
+}
+
+ReactDOM.render(<Carousel />, document.getElementById('carousel'));
