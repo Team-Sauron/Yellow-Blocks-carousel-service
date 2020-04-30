1eb1a63957aeebfc4745eb2a3b8b5f51c0bb8a76
Jacob Orel <jakeorel@gmail.com>
2020-04-30 14:12:59 -0500

    added mongoose db schema and save function

--- Db/index.js
+++ Db/index.js
@@ -1,5 +1,26 @@
-const keys = require('../server/config.js');
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/imageUrls';
 
-  module.exports.getImages: function(folder, keys) {
-
+mongoose.connect(url, (err) => {
+  if (err) {
+    return console.log('FAILED TO CONNECT: ', err);
+  } else {
+    console.log('CONNECTED!');
   }
+});
+
+var Schema = mongoose.Schema;
+
+var imageSchema = new Schema ({
+  url: {type: String, unique: true}
+});
+
+var Images = mongoose.model('Images', imageSchema);
+
+module.exports.save = (input, callback) => {
+  var newImage = new Images ({
+    url: input
+  });
+
+  newImage.save()
+}
