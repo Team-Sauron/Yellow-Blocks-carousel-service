35be5b04e991e2ef6789d5a17ed550547ca3615c
Jacob Orel <jakeorel@gmail.com>
2020-05-01 22:15:37 -0500

    wrote first unit tests

---
+++ __tests__/apiSpec.tests.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+
+const app = require('../Server/index.js');
+
+describe("Test the root path", () => {
+  test("It should respond with a 200", async (done) => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(200);
+    done();
+  });
+});
+
+describe("Test to specified routes", () => {
+  test('response body length should be 1', async (done) => {
+    const response = await request(app).get('/api/images/1');
+    expect(response.body.product).toBe(1);
+    done();
+  });
+});
