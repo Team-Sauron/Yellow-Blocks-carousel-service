ea7a15cfc2c1a25bb175a322d8c4d798b67dadb2
Jacob Orel <jakeorel@gmail.com>
2020-05-04 16:02:20 -0500

    removed comp did mount func

--- Client/components/carousel.jsx
+++ Client/components/carousel.jsx
@@ -6,30 +6,12 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [],
     };
-  }
-
-  componentDidMount() {
-    const wholeUrl = window.location.href;
-    const id = wholeUrl.slice(wholeUrl.indexOf('=') + 1);
-    $.ajax({
-      method: 'GET',
-      url: `/api/images/${id}`,
-      success: (data) => {
-        this.setState({
-          images: data,
-        });
-      },
-    });
   }
 
   render() {
     return (
-      <div>
-        <div />
-        <div>IM INSIDE REACT</div>
-      </div>
+      <div>IM INSIDE REACT</div>
     );
   }
 }
