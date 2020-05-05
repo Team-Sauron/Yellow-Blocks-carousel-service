dde36015e779cf3b5b33632af00c4bb4790033c0
Jacob Orel <jakeorel@gmail.com>
2020-05-05 18:14:24 -0500

    added up and down button put into grid

--- Client/components/ImageBar.jsx
+++ Client/components/ImageBar.jsx
@@ -3,11 +3,23 @@ import styled from 'styled-components';
 import ImageEntry from './ImageEntry.jsx';
 
 const ImageHolder = styled.div`
+  text-align: center;
+`;
+const ScrollUp = styled.button`
+  grid-area: arrowUp
+  cursor: pointer;
+`;
+
+const ScrollDown = styled.button`
+  grid-area: arrowDown;
+  cursor: pointer;
 `;
 
 const ImageBar = ({ images }) => (
   <ImageHolder>
+    <ScrollUp>/\</ScrollUp>
     {Object.values(images).map((image) => <ImageEntry image={image} />)}
+    <ScrollDown>\/</ScrollDown>
   </ImageHolder>
 );
 
