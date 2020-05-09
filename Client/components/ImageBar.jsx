df071d623277d710339989cdc67f2a3a2f436af4
Jacob Orel <jakeorel@gmail.com>
2020-05-08 19:22:47 -0500

    fixed jsx extensions

--- Client/components/ImageBar.jsx
+++ Client/components/ImageBar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import ImageEntry from './ImageEntry.jsx';
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
+import ImageEntry from './ImageEntry';
 
 const ImageBar = ({ images, onClick }) => (
   <div className="ImageHolder">
-    <button className="ScrollUp" type="button">/\</button>
-    {images.map((image, key) => (
+    <button className="ScrollUp" type="button">
+      <FaChevronUp size="18px" />
+    </button>
+    {images.map((image) => (
       <ImageEntry
-        key={key}
+        key={image.id}
         image={image}
         onClick={(e) => onClick(e)}
       />
     ))}
-    <button className="ScrollDown" type="button">\/</button>
+    <button className="ScrollDown" type="button">
+      <FaChevronDown size="18px" />
+    </button>
   </div>
 );
 
