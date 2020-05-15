5e6e0d241191bbe1e2c22226308011873a5a2a11
Jacob Orel <jakeorel@gmail.com>
2020-05-14 19:11:36 -0500

    changed to promise and added supertest.agent

--- __tests__/apiSpec.tests.js
+++ __tests__/apiSpec.tests.js
@@ -2,30 +2,38 @@
 const supertest = require('supertest');
 const app = require('../Server/index.js');
 
-const request = supertest(app);
+const request = supertest.agent(app);
 
 describe('Test back end requests', () => {
-  test('response product should be 1', async (next) => {
-    const response = await request.get('/api/images/1');
-    expect(response.body.product).toBe(1);
-    next();
+  test('response product should be 1', async (done) => {
+    request.get('/api/images/1')
+      .then(((response) => {
+        expect(response.body.product).toBe(1);
+      }));
+    done();
   });
 
-  test('response product to id:12 should be 12', async (next) => {
-    const response = await request.get('/api/images/12');
-    expect(response.body.product).toBe(12);
-    next();
+  test('response product to id:12 should be 12', async (done) => {
+    request.get('/api/images/12')
+      .then((response) => {
+        expect(response.body.product).toBe(12);
+      });
+    done();
   });
 
-  test('it should return product number 1 id is over 100', async (next) => {
-    const response = await request.get('/api/images/101');
-    expect(response.body.product).toBe(1);
-    next();
+  test('it should return product number 1 id is over 100', async (done) => {
+    request.get('/api/images/101')
+      .then((response) => {
+        expect(response.body.product).toBe(1);
+      });
+    done();
   });
 
-  test('it should return product 1 if id is less than 0', async (next) => {
-    const response = await request.get('/api/images/-1');
-    expect(response.body.product).toBe(1);
-    next();
+  test('it should return product 1 if id is less than 0', async (done) => {
+    request.get('/api/images/-1')
+      .then((response) => {
+        expect(response.body.product).toBe(1);
+      });
+    done();
   });
 });
